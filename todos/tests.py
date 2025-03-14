aedda68aca82caa865e5c39d4c0d3d31f1a68898
Jeremy <jeremy@kilsar.com>
2025-03-14 06:37:26 -0400

    Add requirements.txt

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Todo
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(title='first todo', body='todo body')
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, 'first todo')
+        self.assertEqual(self.todo.body, 'todo body')
+        self.assertEqual(str(self.todo), 'first todo')
